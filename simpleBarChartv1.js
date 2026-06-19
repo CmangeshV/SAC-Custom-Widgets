9d2da82f8c972b9ba8b71fb1e58fa0b3b4a6eef2
CmangeshV <102353184+CmangeshV@users.noreply.github.com>
2026-06-19 13:40:13 +0530

    Update simpleBarChartv1.js

--- simpleBarChartv1.js
+++ simpleBarChartv1.js
@@ -6,26 +6,15 @@
             this.innerHTML = "<h2>Check Console</h2>";
         }
 
-       onCustomWidgetAfterUpdate(changedProperties) {
+   onCustomWidgetAfterUpdate(changedProperties) {
 
-    console.log("===== DATA BINDING =====");
-
-    console.log(changedProperties.dataBinding);
-
-    if (changedProperties.dataBinding) {
-
-        console.log("===== MESSAGES =====");
-
-        console.log(changedProperties.dataBinding.messages);
-
-        console.log(
-            JSON.stringify(
-                changedProperties.dataBinding,
-                null,
-                2
-            )
-        );
-    }
+    console.log(
+        JSON.stringify(
+            changedProperties.dataBinding.data,
+            null,
+            2
+        )
+    );
 }
 
     customElements.define(
