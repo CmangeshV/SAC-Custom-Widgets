3b8395384c98fa4e096c8b48a0b29f3198b24538
CmangeshV <102353184+CmangeshV@users.noreply.github.com>
2026-06-19 13:49:17 +0530

    Update simpleBarChartv1.js

--- simpleBarChartv1.js
+++ simpleBarChartv1.js
@@ -6,15 +6,43 @@
             this.innerHTML = "<h2>Check Console</h2>";
         }
 
-   onCustomWidgetAfterUpdate(changedProperties) {
+  onCustomWidgetAfterUpdate(changedProperties) {
 
-    console.log(
-        JSON.stringify(
-            changedProperties.dataBinding.data,
-            null,
-            2
-        )
-    );
+    if (changedProperties.dataBinding?.state === "success") {
+
+        let data = changedProperties.dataBinding.data;
+
+        let maxValue = Math.max(
+            ...data.map(x => x.measures_0.raw)
+        );
+
+        let html = "<h3>Sales by Month</h3>";
+
+        data.forEach(row => {
+
+            let width =
+                (row.measures_0.raw / maxValue) * 300;
+
+            html += `
+                <div style="margin-bottom:10px">
+                    <div>
+                        ${row.dimensions_0.label}
+                        (${row.measures_0.formatted})
+                    </div>
+
+                    <div
+                        style="
+                            background:#4285f4;
+                            height:25px;
+                            width:${width}px;
+                        ">
+                    </div>
+                </div>
+            `;
+        });
+
+        this.innerHTML = html;
+    }
 }
 
     customElements.define(
